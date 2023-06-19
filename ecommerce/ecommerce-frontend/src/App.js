459d9c0f9fc615806e8e1e4d8aee58332fdfe2be
AndreasOun <andreastthk@gmail.com>
2023-06-19 12:07:18 +0300

    Create-React-App

---
+++ ecommerce/ecommerce-frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">store</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
