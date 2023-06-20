0e3579621a51a662a934245acd2b0bf0812073cb
AndreasOun <andreastthk@gmail.com>
2023-06-20 13:44:18 +0300

    Added products

--- ecommerce/ecommerce-frontend/src/App.js
+++ ecommerce/ecommerce-frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from "./data";
+
 
 function App() {
   return (
@@ -6,7 +8,25 @@ function App() {
         <a href="/">store</a>
       </header>
       <main>
-        list products
+        <h1>Featured products</h1>
+        <div className="products">
+          {data.products.map(product => (
+          <div className="product" key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </a>
+            <div className="product-info">
+              <a href={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </a>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to cart</button>
+            </div>
+            </div>
+          ))}
+          </div>
       </main>
     </div>
   );
