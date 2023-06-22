6d3c6eae95f4fb2387e43d3fb0d6014c7a33c43d
AndreasOun <andreastthk@gmail.com>
2023-06-22 20:12:27 +0300

    Loading component + error message

--- ecommerce/ecommerce-frontend/src/screens/ProductScreen.js
+++ ecommerce/ecommerce-frontend/src/screens/ProductScreen.js
@@ -10,6 +10,8 @@ import Button from 'react-bootstrap/Button';
 import Rating from '../components/Rating';
 import ListGroupItem from 'react-bootstrap/esm/ListGroupItem';
 import { Helmet } from 'react-helmet-async';
+import MessageBox from '../components/MessageBox';
+import { getError } from '../utils';
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -42,15 +44,17 @@ function ProductScreen() {
             const result = await axios.get(`/api/products/slug/${slug}`);
             dispatch({ type: 'FETCH_SUCCESS', payload: result.data});
           } catch(err) {
-            dispatch({ type: 'FETCH_FAIL', payload: err.message});
+            dispatch({ type: 'FETCH_FAIL', payload: getError(err)});
           }
         };
         fetchData();
       }, [slug]);
 
     return loading ? (
-      <div>Loading...</div>
-    ) : error ? (
+        <loadingBox />
+      ) : error ? ( 
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : error ? (
       <div>{error}</div>
     ) : (
       <div>
