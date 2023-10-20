d66c1742a49469ce57d77568aa81928c8dec3b8a
AndreasOun <andreastthk@gmail.com>
2023-10-20 22:08:28 +0300

    Ordering by paypal implemented

--- ecommerce/ecommerce-backend/server.js
+++ ecommerce/ecommerce-backend/server.js
@@ -7,6 +7,7 @@ import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
 
+
 dotenv.config();
 
 mongoose
@@ -17,22 +18,26 @@ mongoose
   .catch((err) => {
     console.log(err.message);
   });
-
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api/keys/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+
 
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
-
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
-}); 
+});
 
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
-    console.log(`serve at http://localhost:${port}`);
+  console.log(`serve at http://localhost:${port}`);
 });
--- ecommerce/ecommerce-frontend/src/screens/OrderScreen.js
+++ ecommerce/ecommerce-frontend/src/screens/OrderScreen.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useContext, useEffect, useReducer } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
 import { useNavigate, useParams } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -11,6 +12,7 @@ import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { Store } from '../Store';
 import { getError } from '../utils';
+import { toast } from 'react-toastify';
 
 function reducer(state, action) {
     switch (action.type) {
@@ -20,6 +22,14 @@ function reducer(state, action) {
         return { ...state, loading: false, order: action.payload, error: '' };
       case 'FETCH_FAIL':
         return { ...state, loading: false, error: action.payload };
+      case 'PAY_REQUEST':
+          return { ...state, loadingPay: true };
+      case 'PAY_SUCCESS':
+          return { ...state, loadingPay: false, successPay: true };
+      case 'PAY_FAIL':
+          return { ...state, loadingPay: false };
+      case 'PAY_RESET':
+          return { ...state, loadingPay: false, successPay: false };
   
       default:
         return state;
@@ -28,17 +38,59 @@ function reducer(state, action) {
   export default function OrderScreen() {
     const { state } = useContext(Store);
     const { userInfo } = state;
-  
     const params = useParams();
     const { id: orderId } = params;
     const navigate = useNavigate();
-  
-    const [{ loading, error, order }, dispatch] = useReducer(reducer, {
+
+    const [{ loading, error, order, successPay, loadingPay }, dispatch] =
+    useReducer(reducer, {
       loading: true,
       order: {},
       error: '',
+      successPay: false,
+      loadingPay: false,
     });
+
+
+    const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
+
+    function createOrder(data, actions) {
+      return actions.order
+        .create({
+          purchase_units: [
+            {
+              amount: { value: order.totalPrice },
+            },
+          ],
+        })
+        .then((orderID) => {
+          return orderID;
+        });
+    }
   
+    function onApprove(data, actions) {
+      return actions.order.capture().then(async function (details) {
+        try {
+          dispatch({ type: 'PAY_REQUEST' });
+          const { data } = await axios.put(
+            `/api/orders/${order._id}/pay`,
+            details,
+            {
+              headers: { authorization: `Bearer ${userInfo.token}` },
+            }
+          );
+          dispatch({ type: 'PAY_SUCCESS', payload: data });
+          toast.success('Order is paid');
+        } catch (err) {
+          dispatch({ type: 'PAY_FAIL', payload: getError(err) });
+          toast.error(getError(err));
+        }
+      });
+    }
+    function onError(err) {
+      toast.error(getError(err));
+    }
+
     useEffect(() => {
       const fetchOrder = async () => {
         try {
@@ -55,10 +107,28 @@ function reducer(state, action) {
       if (!userInfo) {
         return navigate('/signin');
       }
-      if (!order._id || (order._id && order._id !== orderId)) {
+      if (!order._id || successPay || (order._id && order._id !== orderId)) {
         fetchOrder();
+        if (successPay) {
+          dispatch({ type: 'PAY_RESET' });
+        }
+      } else {
+        const loadPaypalScript = async () => {
+          const { data: clientId } = await axios.get('/api/keys/paypal', {
+            headers: { authorization: `Bearer ${userInfo.token}` },
+          });
+          paypalDispatch({
+            type: 'resetOptions',
+            value: {
+              'client-id': clientId,
+              currency: 'USD',
+            },
+          });
+          paypalDispatch({ type: 'setLoadingStatus', value: 'pending' });
+        };
+        loadPaypalScript();
       }
-    }, [order, userInfo, orderId, navigate]);
+    }, [order, userInfo, orderId, navigate, paypalDispatch, successPay]);
     return loading ? (
       <LoadingBox></LoadingBox>
     ) : error ? (
@@ -106,30 +176,30 @@ function reducer(state, action) {
             </Card>
   
             <Card className="mb-3">
-              <Card.Body>
-                <Card.Title>Items</Card.Title>
-                <ListGroup variant="flush">
-                  {order.orderItems.map((item) => (
-                    <ListGroup.Item key={item._id}>
-                      <Row className="align-items-center">
-                        <Col md={6}>
-                          <img
-                            src={item.image}
-                            alt={item.name}
-                            className="img-fluid rounded img-thumbnail"
-                          ></img>{' '}
-                          <Link to={`/product/${item.slug}`}>{item.name}</Link>
-                        </Col>
-                        <Col md={3}>
-                          <span>{item.quantity}</span>
-                        </Col>
-                        <Col md={3}>${item.price}</Col>
-                      </Row>
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              </Card.Body>
-            </Card>
+            <Card.Body>
+              <Card.Title>Items</Card.Title>
+              <ListGroup variant="flush">
+                {order?.orderItems?.map((item) => (
+                  <ListGroup.Item key={item._id}>
+                    <Row className="align-items-center">
+                      <Col md={6}>
+                        <img
+                          src={item.image}
+                          alt={item.name}
+                          className="img-fluid rounded img-thumbnail"
+                        />{" "}
+                        <Link to={`/product/${item.slug}`}>{item.name}</Link>
+                      </Col>
+                      <Col md={3}>
+                        <span>{item.quantity}</span>
+                      </Col>
+                      <Col md={3}>${item.price}</Col>
+                    </Row>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Card.Body>
+          </Card>
           </Col>
           <Col md={4}>
             <Card className="mb-3">
@@ -139,7 +209,7 @@ function reducer(state, action) {
                   <ListGroup.Item>
                     <Row>
                       <Col>Items</Col>
-                      <Col>${order.itemsPrice.toFixed(2)}</Col>
+                      <Col>${Number(order.itemsPrice).toFixed(2)}</Col>
                       </Row>
                 </ListGroup.Item>
                 <ListGroup.Item>
@@ -164,6 +234,22 @@ function reducer(state, action) {
                     </Col>
                   </Row>
                 </ListGroup.Item>
+                {!order.isPaid && (
+                  <ListGroup.Item>
+                    {isPending ? (
+                      <LoadingBox />
+                    ) : (
+                      <div>
+                        <PayPalButtons
+                          createOrder={createOrder}
+                          onApprove={onApprove}
+                          onError={onError}
+                        ></PayPalButtons>
+                      </div>
+                    )}
+                    {loadingPay && <LoadingBox></LoadingBox>}
+                  </ListGroup.Item>
+                )}
               </ListGroup>
             </Card.Body>
           </Card>
