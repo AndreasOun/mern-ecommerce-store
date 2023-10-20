0a97e873311dfdb500aec5a1a77882a4685685be
AndreasOun <andreastthk@gmail.com>
2023-10-20 23:27:20 +0300

    Implemented order history screen

--- ecommerce/ecommerce-backend/routes/orderRoutes.js
+++ ecommerce/ecommerce-backend/routes/orderRoutes.js
@@ -21,19 +21,15 @@ orderRouter.post(
     res.status(201).send({ message: 'New Order Created', order });
   })
 );
+
 orderRouter.get(
-  '/:id',
+  '/mine',
   isAuth,
   expressAsyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id);
-    if (order) {
-      res.send(order);
-    } else {
-      res.status(404).send({ message: 'Order Not Found' });
-    }
+    const orders = await Order.find({ user: req.user._id });
+    res.send(orders);
   })
 );
-
 orderRouter.put(
   '/:id/pay',
   isAuth,
