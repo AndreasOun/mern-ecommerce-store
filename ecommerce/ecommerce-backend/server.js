4ea9fb7f027e0ec1d738bdfef4b03f1b0abf14e1
AndreasOun <andreastthk@gmail.com>
2023-11-03 18:50:27 +0200

    another bug fix

--- ecommerce/ecommerce-backend/server.js
+++ ecommerce/ecommerce-backend/server.js
@@ -34,9 +34,9 @@ app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '/ecommerce-frontend/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, '/ecommerce-frontend/build/index.html'))
 );
 
 app.use((err, req, res, next) => {
